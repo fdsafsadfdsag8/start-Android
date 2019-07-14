e0d871a036d8127069ede01b044c74a1354abf22
bit-savoki <401471767@qq.com>
2019-07-14 09:02:26 +0800

    day3的作业

---
+++ app/src/main/java/com/example/chapter3/homework/Ch3Ex3Activity.java
@@ -0,0 +1,91 @@
+package com.example.chapter3.homework;
+
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+/**
+ * 使用 ViewPager 和 Fragment 做一个简单版的好友列表界面
+ * 1. 使用 ViewPager 和 Fragment 做个可滑动界面
+ * 2. 使用 TabLayout 添加 Tab 支持
+ * 3. 对于好友列表 Fragment，使用 Lottie 实现 Loading 效果，在 5s 后展示实际的列表，要求这里的动效是淡入淡出
+ */
+public class Ch3Ex3Activity extends AppCompatActivity {
+
+    //private ListView lv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ch3ex3);
+
+        // TODO: ex3-1. 添加 ViewPager 和 Fragment 做可滑动界面
+        ViewPager pager=findViewById(R.id.vp);
+        pager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int i) {
+                return new PlaceholderFragment();
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+
+            @Nullable
+            @Override
+            public CharSequence getPageTitle(int position) {
+                switch (position){
+                    case 0:
+                        return "好友页面";
+                    case 1:
+                        return "其他";
+                }
+                return null;
+            }
+        });
+
+        // TODO: ex3-2, 添加 TabLayout 支持 Tab
+        TabLayout tab=findViewById(R.id.tab);
+        tab.setupWithViewPager(pager);
+
+        /*
+        //listview
+        lv=(ListView) findViewById(R.);
+        lv.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return 10;
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                return null;
+            }
+
+            @Override
+            public int getItemViewType(int position) {
+                return 0;
+            }
+
+        });*/
+    }
+}
